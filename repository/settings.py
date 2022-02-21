2e07ee90ad208c9a8580ffd5cde794d6c66091e6
Alwizain <alwizainalmastrigreisian@gmail.com>
2022-02-21 23:21:47 +0700

    update lib storages on settings.py

--- repository/settings.py
+++ repository/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'storages',
     'django_extensions',
+    # 'storages',
 
     #apps
     'resources.apps.ResourcesConfig',
